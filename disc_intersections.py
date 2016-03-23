6496a3e0b7ce848136d793841efe54d9c2b57e87
Martin Breining <martin.breining@gmail.com>
2016-03-22 22:05:23 -0700

    Add solution using merge portion of mergesort

--- disc_intersections.py
+++ disc_intersections.py
@@ -9,7 +9,41 @@ http://stackoverflow.com/questions/4801242/algorithm-to-calculate-number-of-inte
 
 def solution(A):
     """
+    A solution in linear time using the merge portion of the mergesort algorithm.
     >>> solution([1, 5, 2, 1, 4, 0])
+    11
+    """
+    lo_bounds = list()
+    hi_bounds = list()
+    for i, a in enumerate(A):
+        lo_bounds.append(i-a)
+        hi_bounds.append(i+a)
+
+    lo_bounds.sort()
+    hi_bounds.sort()
+
+    overlaps, intersects = 0, 0
+    i, j = 0, 0
+    while i < len(lo_bounds) and j < len(hi_bounds):
+        if lo_bounds[i] <= hi_bounds[j]:
+            overlaps += 1
+            i += 1
+        else:
+            overlaps -= 1
+            j += 1
+        intersects += overlaps // 2
+
+    while j < len(hi_bounds):
+        overlaps -= 1
+        j += 1
+        intersects += overlaps // 2
+
+    return intersects
+
+
+def solution1(A):
+    """
+    >>> solution1([1, 5, 2, 1, 4, 0])
     11
     """
     low_bounds = [0] * len(A)
@@ -31,6 +65,51 @@ def solution(A):
     if intersects > 10000000:
         return -1
     return intersects
+
+
+def _binsearch(arr, x, lo, hi):
+    """
+    >>> _binsearch([-4, -1, 0, 0, 2, 5], 0, 0, 5)
+    4
+    >>> _binsearch([-4, -1, 0, 0, 2, 5], 3, 0, 5)
+    5
+    """
+    while lo <= hi:
+        mid = (lo+hi)//2
+        if x >= arr[mid]:
+            lo = mid+1
+        else:
+            hi = mid-1
+    return lo
+
+
+def solution2(A):
+    """
+    O(nlog(n)) time complexity using binary search.
+    >>> solution2([1, 5, 2, 1, 4, 0])
+    11
+    """
+    # Create list of intervals
+    intervals = list()
+    for i, rad in enumerate(A):
+        intervals.append([i-rad, i+rad])
+
+    # Sort list by lower bound
+    intervals.sort()
+
+    # For each interval, find number of intersections between low and high bounds
+    # of interval using binary search
+    intersects = 0
+    lo_bounds = [i[0] for i in intervals]
+    for i, (lo, hi) in enumerate(intervals):
+        insert_idx = _binsearch(lo_bounds, hi, i, len(intervals)-1)
+        intersects += insert_idx - i - 1
+
+    if intersects > 10000000:
+        return -1
+
+    return intersects
+
 
 if __name__ == "__main__":
     import doctest
