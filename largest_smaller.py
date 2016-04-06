659485ee58f84b86002cb7db465fe0a59b80bd24
Martin Breining <martin.breining@gmail.com>
2016-04-05 23:42:04 -0700

    Minor comment update

--- largest_smaller.py
+++ largest_smaller.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python
 
 """
-https://www.pramp.com/question/pK6A4GA5YES09qKmqG33
-
 Given a root of a binary search tree and a key x, find the largest key in the tree that
 is smaller than x.
 
 Example: if an in-order list of all keys in the tree is {2, 3, 4, 7, 17, 19, 21, 35, 89}
 and x is 19, the biggest key that is smaller than x is 17.
+
+https://www.pramp.com/question/pK6A4GA5YES09qKmqG33
 """
 
 
