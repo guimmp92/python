426f231d3295519c1f92259f896e2f63858b54be
Martin Breining <martin.breining@gmail.com>
2016-04-11 23:07:52 -0700

    Add reverse linked list in groupd of size k

---
+++ reverse_linked_list2.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+
+"""
+Reverse a linked list in groups of size k.
+
+http://www.geeksforgeeks.org/reverse-a-list-in-groups-of-given-size/
+"""
+from linked_list import randlinkedlist
+
+ll = randlinkedlist()
+print "The linked list: {0}".format(ll)
+
+
+def reverse_linked_list(head, k):
+    node = head
+    prev, next = None, None
+    count = 0
+    while node and count < k:
+        next = node.next
+        node.next = prev
+        prev, node = node, next
+        count += 1
+    if next:
+        head.next = reverse_linked_list(next, k)
+    return prev
+
+
+ll.head = reverse_linked_list(ll.head, 3)
+print "The reversed linked list (iterative): {0}".format(ll)
