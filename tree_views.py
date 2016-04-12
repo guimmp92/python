05ea83939a173f05935276205d6d5a04629e4051
Martin Breining <martin.breining@gmail.com>
2016-04-11 17:13:24 -0700

    Add right, top and bottom binary tree views

---
+++ tree_views.py
@@ -0,0 +1,119 @@
+#!/usr/bin/python
+
+"""
+Print left, right, top and bottom view of binary tree.
+"""
+from collections import deque
+from bintree import Node
+
+
+def visit_left_view(node, prev_level, cur_level):
+    if not node:
+        return
+    if cur_level > prev_level[0]:
+        print node.key
+        prev_level[0] = cur_level
+    visit_left_view(node.left, prev_level, cur_level+1)
+    visit_left_view(node.right, prev_level, cur_level+1)
+
+
+def left_view_recursive(root):
+    prev_level = [0]
+    visit_left_view(root, prev_level, 1)
+
+
+def left_view_iterative(root):
+    queue = deque()
+    queue.appendleft((root, 1))
+    prev_level = 0
+    while queue:
+        node, level = queue.pop()
+        if level > prev_level:
+            print node.key
+            prev_level = level
+        if node.left:
+            queue.appendleft((node.left, level+1))
+        if node.right:
+            queue.appendleft((node.right, level+1))
+
+
+def visit_right_view(node, prev_level, cur_level):
+    if not node:
+        return
+    if cur_level > prev_level[0]:
+        print node.key
+        prev_level[0] = cur_level
+    visit_right_view(node.right, prev_level, cur_level+1)
+    visit_right_view(node.left, prev_level, cur_level+1)
+
+
+def right_view_recursive(root):
+    prev_level = [0]
+    visit_right_view(root, prev_level, 1)
+
+
+def top_view_iterative(root):
+    queue = deque()
+    queue.appendleft((root, [0, 0], 0))
+    print root.key
+    while queue:
+        node, top_distance, cur_distance = queue.pop()
+        if cur_distance < top_distance[0]:
+            print node.key
+            top_distance[0] = cur_distance
+        elif cur_distance > top_distance[1]:
+            print node.key
+            top_distance[1] = cur_distance
+        if node.left:
+            queue.appendleft((node.left, top_distance, cur_distance-1))
+        if node.right:
+            queue.appendleft((node.right, top_distance, cur_distance+1))
+
+
+def bottom_view_iterative(root):
+    queue = deque()
+    distances = dict()
+    queue.append((root, 0))  # node and its horizontal distance
+    while queue:
+        node, hd = queue.pop()
+        distances[hd] = node.key
+        if node.left:
+            queue.appendleft((node.left, hd-1))
+        if node.right:
+            queue.appendleft((node.right, hd+1))
+    for hd in sorted(distances):
+        print distances[hd]
+
+
+if __name__ == "__main__":
+    root = Node(12)
+    root.left = Node(10)
+    root.right = Node(20)
+    root.right.left = Node(25)
+    root.right.right = Node(40)
+    root.right.left.right = Node(3)
+
+    print "Left view (recursive)"
+    left_view_recursive(root)
+
+    print "Left view (iterative)"
+    left_view_iterative(root)
+
+    print "Right view (recursive)"
+    right_view_recursive(root)
+
+    print "Top view (iterative)"
+    top_view_iterative(root)
+
+    root = Node(1)
+    root.left = Node(2)
+    root.right = Node(3)
+    root.left.right = Node(4)
+    root.left.right.right = Node(5)
+    root.left.right.right.right = Node(6)
+
+    print "Top view (iterative)"
+    top_view_iterative(root)
+
+    print "Bottom view (iterative)"
+    bottom_view_iterative(root)
