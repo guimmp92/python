c6157da49f0accd05f6dc330c3574259ce745400
Martin Breining <martin.breining@gmail.com>
2016-04-13 22:00:02 -0700

    Better use of deque

--- first_nonrepeat_char.py
+++ first_nonrepeat_char.py
@@ -3,28 +3,27 @@
 """
 Given a stream of characters, find the first non-repeating character.
 """
-
 from collections import deque
+
+
+def first_non_repeat(stream):
+    repeated = [0] * 256  # ascii
+    non_repeated = deque()
+    try:
+        while True:
+            c = next(stream)
+            print "\nNext character: {0}".format(c)
+            if not repeated[ord(c)]:
+                if c in non_repeated:
+                    non_repeated.remove(c)
+                    repeated[ord(c)] = 1
+                else:
+                    non_repeated.appendleft(c)
+            print "First non repeated character: {0}".format(non_repeated[-1])
+    except StopIteration:
+        print "\nEnd of stream"
 
 print "Enter a string a ascii characters"
 data = raw_input()  # "abcda1234bc45a1"
 stream = (c for c in data)
-
-repeating_chars = []
-non_repeating_chars = deque()
-
-
-def first_non_repeat_char():
-    next_char = stream.next()
-
-    if next_char not in repeating_chars:
-        if next_char in non_repeating_chars:
-            non_repeating_chars.remove(next_char)
-            repeating_chars.append(next_char)
-        else:
-            non_repeating_chars.append(next_char)
-
-    return non_repeating_chars[0]
-
-for i in range(len(data)):
-    print first_non_repeat_char()
+first_non_repeat(stream)
