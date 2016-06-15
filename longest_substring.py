b8c897c3aef951e2ce303c31702903085d5e2f7a
Martin Breining <martin.breining@gmail.com>
2016-06-15 16:39:01 -0700

    Fix solution

--- longest_substring.py
+++ longest_substring.py
@@ -8,9 +8,13 @@ If K is 2, the longest substring can be "aabb".
 If K is 3, the longest substring can be "aabbcc".
 
 http://blog.gainlo.co/index.php/2016/04/12/find-the-longest-substring-with-k-unique-characters/
+http://www.geeksforgeeks.org/find-the-longest-substring-with-k-unique-characters-in-a-given-string/
 """
-from collections import deque
 from collections import defaultdict
+
+
+def count_uniques(uniques):
+    return len([e for e in uniques if e != 0])
 
 
 def solution(text, k):
@@ -23,54 +27,25 @@ def solution(text, k):
     'aabbcc'
     >>> solution("abacbebebe", 3)
     'cbebebe'
+    >>> solution("abacbbeb", 3)
+    'abacbb'
     >>> solution("ababebebe", 4)
     ''
     """
-    longest = ""
-    start = 0
-    tracker = defaultdict(list)
-    for i, c in enumerate(text):  # loop invariant: tracker contains k or less elements
-        if len(tracker) < k:
-            tracker[c].append(i)
-        else:
-            if c not in tracker:  # remove trailing character
-                new_start = max(tracker[text[start]])
-                tracker.pop(text[start])
-                start = new_start+1
-            tracker[c].append(i)
-            if i-start+1 > len(longest):
-                longest = text[start:i+1]
-    return longest
-
-
-def incorrect(text, k):
-    """
-    >>> incorrect("aabbccdd", 1)
-    'aa'
-    >>> incorrect("aabbccdd", 2)
-    'aabb'
-    >>> incorrect("aabbaccd", 3)
-    'aabbacc'
-    >>> incorrect("abacbebebe", 3)
-    'cbebebe'
-    """
-    longest = ""
-    queue = deque()
-    indices = dict()
-    for i in range(len(text)):
-        if len(queue) < k:
-            if text[i] not in queue:
-                queue.appendleft(text[i])
-                indices[text[i]] = i
-        else:
-            if text[i] in queue:
-                start_index = indices[queue[-1]]
-                if i - start_index + 1 > len(longest):
-                    longest = text[start_index:i+1]
-            else:
-                char = queue.pop()
-                del indices[char]
-    return longest
+    res = ""
+    maxsofar = 0
+    i = 0
+    uniques = [0] * 256
+    for j in range(len(text)):
+        uniques[ord(text[j])] += 1
+        while count_uniques(uniques) > k:
+            uniques[ord(text[i])] -= 1
+            i += 1
+        if count_uniques(uniques) == k:
+            if j-i+1 > maxsofar:
+                maxsofar = j-i+1
+                res = text[i:j+1]
+    return res
 
 
 if __name__ == "__main__":
