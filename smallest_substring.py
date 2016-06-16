f2325bab53c1bdd1e26fdedb36ea4608597a9933
Martin Breining <martin.breining@gmail.com>
2016-06-15 17:31:05 -0700

    Add solution with window

--- smallest_substring.py
+++ smallest_substring.py
@@ -38,6 +38,42 @@ def solution(arr, text):
     return minsofar
 
 
+def is_valid(window, arr):
+    return all([window[ord(c)] > 0 for c in arr])
+
+
+def solution2(arr, text):
+    """
+    Time complexity: O(n)
+    >>> solution2(['x', 'y', 'z'], "xyyzyzyx")
+    'zyx'
+    >>> solution2(['x', 'y', 'z'], "xyyzxyzyx")
+    'yzx'
+    >>> solution2(['x', 'y', 'z'], "xyyyyzyzy")
+    'xyyyyz'
+    >>> solution2(['x', 'y', 'z'], "xxyxyxyxxxyxxxz")
+    'yxxxz'
+    >>> solution2(['t', 'i', 's'], "this is a test string")
+    'this'
+    """
+    res = ""
+    minsofar = len(text)
+    lo = 0
+    window = [0] * 256
+    for hi in range(len(text)):
+        # Add new character to window
+        window[ord(text[hi])] += 1
+
+        # Remove trailing characters from window and check if we have a new smallest
+        while is_valid(window, arr):
+            if hi-lo+1 < minsofar:
+                minsofar = hi-lo+1
+                res = text[lo:hi+1]
+            window[ord(text[lo])] -= 1
+            lo += 1
+    return res
+
+
 if __name__ == "__main__":
     import doctest
     doctest.testmod()
