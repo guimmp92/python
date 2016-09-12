c50b5bafe0b11978078468f414a7cbdebf1fa589
Martin Breining <martin.breining@gmail.com>
2016-09-12 13:01:54 -0700

    Add vim modeline; Small refactoring

--- first_nonrepeat_char.py
+++ first_nonrepeat_char.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python
+# vim: foldlevel=0
 
 """
 Given a stream of characters, find the first non-repeating character.
@@ -8,22 +9,24 @@ from collections import deque
 
 def first_non_repeat(stream):
     repeated = [0] * 256  # ascii
-    non_repeated = deque()
-    try:
-        while True:
+    non_repeated = deque()  # 256 max size
+    while True:
+        try:
             c = next(stream)
             print "\nNext character: {0}".format(c)
             if not repeated[ord(c)]:
-                if c in non_repeated:
+                if c not in non_repeated:
+                    non_repeated.appendleft(c)
+                else:
                     non_repeated.remove(c)
                     repeated[ord(c)] = 1
-                else:
-                    non_repeated.appendleft(c)
             print "First non repeated character: {0}".format(non_repeated[-1])
-    except StopIteration:
-        print "\nEnd of stream"
+        except StopIteration:
+            print "\nEnd of stream"
+            break
 
-print "Enter a string a ascii characters"
+print "Enter a string as ascii characters"
 data = raw_input()  # "abcda1234bc45a1"
 stream = (c for c in data)
 first_non_repeat(stream)
+#solution(stream)
