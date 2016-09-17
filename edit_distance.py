86f52b6d11adb5916028ae6c31d9a4423d4db3c8
Martin Breining <martin.breining@gmail.com>
2016-09-17 15:35:27 -0700

    Add vim modeline; Add top-down solution

--- edit_distance.py
+++ edit_distance.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python
+# vim: foldlevel=0
 
 """
 Given two strings str1 and str2 and below operations that can be performed on str1.
@@ -70,6 +71,39 @@ def dp(str1, str2):
                 memo[i][j] = 1 + min(memo[i][j-1], memo[i-1][j], memo[i-1][j-1])
 
     return memo[len(str1)-1][len(str2)-1]
+
+
+def rec(str1, str2, m, n, memo):
+    if memo[m][n] == -1:
+        if m == 0 and n == 0:
+            memo[m][n] = 0
+        elif m == 0:
+            memo[m][n] = n
+        elif n == 0:
+            memo[m][n] = m
+        elif str1[m-1] == str2[n-1]:
+            memo[m][n] = rec(str1, str2, m-1, n-1, memo)
+        else:
+            memo[m][n] = 1 + min(rec(str1, str2, m-1, n-1, memo),
+                                 rec(str1, str2, m-1, n, memo),
+                                 rec(str1, str2, m, n-1, memo))
+    return memo[m][n]
+
+
+def dp2(str1, str2):
+    """
+    Top-down dynamic programming.
+    Time complexity: O(m*n)
+    Space complexity: O(m*n)
+    >>> dp2("geek", "gesek")
+    1
+    >>> dp2("cat", "cut")
+    1
+    >>> dp2("sunday", "saturday")
+    3
+    """
+    memo = [[-1 for i in range(len(str2)+1)] for j in range(len(str1)+1)]
+    return rec(str1, str2, len(str1), len(str2), memo)
 
 
 if __name__ == "__main__":
