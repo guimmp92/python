a295f5a407ac7d240e98f12c9d808c8de42f5176
Martin Breining <martin.breining@gmail.com>
2016-09-17 23:32:30 -0700

    Add vim modeline; Simplify code

--- longest_ascending.py
+++ longest_ascending.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python
+# vim: foldlevel=0
 
 """
-Given a sequence of numbers, write a function to find the length of a longest increasing
-subsequence of the given sequence without the consecutive constraint.
+Given a sequence of numbers, write a function to find the length of a longest
+increasing subsequence of the given sequence without the consecutive constraint.
 """
 
 
@@ -10,12 +11,14 @@ def lis(arr, i):
     """ list(i) is the max length of seq ending at i, with arr[i] being part of it.
         list(i) = 1 + max(lis(j)) if arr[j] < arr[i] else 1
     """
-    maxendinghere = 1
+    if i == 0:
+        return 1
+    curmax = 0
     for j in range(i):
         lis_j = lis(arr, j)
-        if lis_j+1 > maxendinghere and arr[i] > arr[j]:
-            maxendinghere = lis_j+1
-    return maxendinghere
+        if arr[j] < arr[i]:
+            curmax = max(curmax, lis_j)
+    return 1 + curmax
 
 
 def solution1(arr):
@@ -34,13 +37,13 @@ def solution1(arr):
 
 
 def lis2(arr, i, memo):
-    if not memo.get(i):
-        maxendinghere = 1
+    if memo[i] == -1:
+        curmax = 0
         for j in range(i):
             lis_j = lis2(arr, j, memo)
-            if lis_j+1 > maxendinghere and arr[i] > arr[j]:
-                maxendinghere = lis_j+1
-        memo[i] = maxendinghere
+            if arr[j] < arr[i]:
+                curmax = max(curmax, lis_j)
+        memo[i] = 1 + curmax
     return memo[i]
 
 
@@ -56,53 +59,9 @@ def solution2(arr):
     >>> solution2([1, 20, 5, 15, 40, 10, -5, -2])
     4
     """
-    memo = dict()
-    return max(lis2(arr, i, memo) for i in range(len(arr)))
-
-
-def _binsearch(arr, x):
-    lo, hi = 0, len(arr)-1
-    while lo <= hi:
-        mid = (lo+hi)//2
-        if x >= arr[mid]:
-            lo = mid+1
-        elif x < arr[mid]:
-            hi = mid-1
-    return lo
-
-
-def solution3(arr):
-    """
-    Iterative. Pretty ugly.
-    >>> solution3([1, 3, -1])
-    2
-    >>> solution3([1, 3, 2, 4, 5, 1])
-    4
-    >>> solution3([1, 20, 5, 15, 40, 10, -5, -2])
-    4
-    """
-    subs = []
-    for i, num in enumerate(arr):
-        sub, insert_idx = None, None
-        for s in subs:
-            idx = _binsearch(s, num)  # we assume that there are no duplicates in arr
-            if idx == 0:  # num goes to the left of the subsequence
-                continue
-            if not insert_idx or (insert_idx and idx > insert_idx):
-                insert_idx, sub = idx, s
-        if sub:
-            # The number goes at the end of an existing subsequence, append it.
-            if insert_idx == len(sub)-1:
-                s.append(num)
-            # The number goes in the middle of an existing subsequence, create a new
-            # subsequence.
-            else:
-                subs.append(s[:insert_idx] + [num])
-        else:
-            # The number is smaller than any number seen so far, create a new
-            # subsequence.
-            subs.append([num])
-    return max(len(s) for s in subs)
+    memo = [-1] * len(arr)
+    lis2(arr, len(arr)-1, memo)
+    return max(memo)
 
 
 if __name__ == "__main__":
