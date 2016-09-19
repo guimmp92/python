c5d6e8c6d9cbbf99023d34a2ac6900c54a770cae
Martin Breining <martin.breining@gmail.com>
2016-09-18 20:59:51 -0700

    Add solution w/ rolling hash

--- anagrams.py
+++ anagrams.py
@@ -13,7 +13,6 @@ from collections import defaultdict
 
 def solution(a, b):
     """
-    Time complexity:
     >>> solution("coding interview questions", "weivretni")
     True
     >>> solution("coding interviw questions", "weivretni")
@@ -38,6 +37,29 @@ def solution(a, b):
 
     return False
 
+
+def solution1(a, b):
+    """
+    >>> solution1("coding interview questions", "weivretni")
+    True
+    >>> solution1("coding interviw questions", "weivretni")
+    False
+    >>> solution1("coding interview questions", "abcde")
+    False
+    """
+    target_hash, rolling_hash = 0, 0
+    for i in range(len(b)):
+        target_hash += ord(b[i]) // 101
+        rolling_hash += ord(a[i]) // 101
+
+    i = 0
+    for j in range(len(b), len(a)):
+        if rolling_hash == target_hash:
+            return True
+        rolling_hash += ord(a[j]) // 101
+        rolling_hash -= ord(a[i]) // 101
+        i += 1
+    return False
 
 if __name__ == "__main__":
     import doctest
