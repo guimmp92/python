567eeaa7a8f0a5432c2926a9f10e58dea8adb063
Martin Breining <martin.breining@gmail.com>
2016-09-30 09:59:31 -0700

    Cleanup code

--- reconstruct_serialized_tree.py
+++ reconstruct_serialized_tree.py
@@ -1,23 +1,22 @@
 #!/usr/bin/python
+# vim: foldlevel=0
 
 """
 Given a serialized tree, reconstruct the tree in memory and return the root of the tree.
+
 https://www.reddit.com/r/cscareerquestions/comments/111yyj/what_are_some_good_technical_things_to_know_for/c6imw32
 """
-
 from collections import namedtuple
+
 
 serialized_tree = (('b', 'c'), ('b', 'd'), ('a', 'b'), ('a', 'f'), ('d', 'g'))
 tree = {}
 node = namedtuple('Node', ('value', 'children'))
-root = None
 
 for link in serialized_tree:
     parent, child = link
-    if parent not in tree:
-        tree.setdefault(parent, node(value=parent, children=[]))
-    if child not in tree:
-        tree.setdefault(child, node(value=child, children=[]))
+    tree.setdefault(parent, node(value=parent, children=[]))
+    tree.setdefault(child, node(value=child, children=[]))
     tree[parent].children.append(tree[child])
 
 for key in tree.keys():
@@ -27,3 +26,4 @@ for key in tree.keys():
         del tree[child.value]  # delete the key
 
 print tree.popitem()
+# Alternatively: print next(n[1] for n in tree.items())
