7c200f98f2d6b51755bb179119107ca0105d179b
Martin Breining <martin.breining@gmail.com>
2016-10-01 21:44:57 -0700

    Change file permission

--- activity_selection.py
+++ activity_selection.py
@@ -4,7 +4,7 @@
 """
 You are given n activities with their start and finish times.
 Select the maximum number of activities that can be performed by a single
- person, assuming that a person can only work on a single activity at a time.
+person, assuming that a person can only work on a single activity at a time.
 
 http://www.geeksforgeeks.org/greedy-algorithms-set-1-activity-selection-problem/
 """
