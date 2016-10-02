1cad8b2bd2943a71102c0d3e0771e33c23295d67
Martin Breining <martin.breining@gmail.com>
2016-10-01 22:08:03 -0700

    Fix bug

--- time_planner.py
+++ time_planner.py
@@ -8,29 +8,22 @@ https://www.pramp.com/question/3QnxW6xoPLTNl5jX5Lg1
 """
 
 
-def compute_overlap(A, B):
-    start, end = max(A[0], B[0]), min(A[1], B[1])
-    if start < end:
-        return [start, end]
-
-
 def solution(dur, A, B):
     """
-    >>> solution(30, [[0, 10], [50, 65], [70, 110]], [[5, 20], [25, 50], [75, 105]])
-    [75, 105]
+    >>> solution(30, [(0, 10), (50, 65), (70, 110)], [(5, 20), (25, 50), (75, 105)])
+    (75, 105)
     """
-    res = []
+    res = None
     i, j = 0, 0
     while i < len(A) and j < len(B):
-        overlap = compute_overlap(A[i], B[j])
-        if overlap and overlap[1] - overlap[0] >= dur:
-            res = [overlap[0], overlap[0]+dur]
+        overlap = (max(A[i][0], B[j][0]), min(A[i][1], B[j][1]))
+        if overlap[1] - overlap[0] >= dur:
+            res = overlap
             break
+        if A[i][1] <= B[j][1]:  # which one ends first?
+            i += 1
         else:
-            if A[i][0] <= B[j][0]:
-                i += 1
-            else:
-                j += 1
+            j += 1
     return res
 
 
